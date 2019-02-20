fe23334f85c3cebe5f886d7f7271b95f98b6537e
Neil Chulpongsatorn <neil_ninja@hotmail.com>
2019-02-20 13:57:55 -0700

    Touch interaction initialize, still need work.
    Pupper faces implemented, need interactions.

--- Project1-Pet/MainPage.xaml.cs
+++ Project1-Pet/MainPage.xaml.cs
@@ -1,30 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Project1_Pet
 {
+
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Ellipse touchPoint;
+        SolidColorBrush Red = new SolidColorBrush(Colors.Red);
+
         public MainPage()
         {
+
+
             this.InitializeComponent();
+
+            this.Container.PointerPressed += OnPointerPressed;
+            this.Container.PointerMoved += OnPointerMoved;
+            this.Container.PointerReleased += OnPointerReleased;
+
+            this.touchPoint = new Ellipse()
+            {
+                Width = 100,
+                Height = 100,
+                Fill = Red,
+                RenderTransform = new CompositeTransform()
+            };
+        }
+
+        void OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+
+            (this.touchPoint.RenderTransform as CompositeTransform).TranslateX = e.GetCurrentPoint(this.Container).Position.X - this.touchPoint.Width / 2;
+            (this.touchPoint.RenderTransform as CompositeTransform).TranslateY = e.GetCurrentPoint(this.Container).Position.Y - this.touchPoint.Height / 2;
+
+            this.Container.Children.Add(this.touchPoint);
+
+            if (!this.Container.Children.Contains(this.touchPoint))
+            {
+                this.Container.Children.Add(this.touchPoint);
+            }
+        }
+
+        void OnPointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            this.Container.Children.Remove(this.touchPoint);
+        }
+
+        void OnPointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            (this.touchPoint.RenderTransform as CompositeTransform).TranslateX = e.GetCurrentPoint(this.Container).Position.X - this.touchPoint.Width / 2;
+            (this.touchPoint.RenderTransform as CompositeTransform).TranslateY = e.GetCurrentPoint(this.Container).Position.Y - this.touchPoint.Height / 2;
+
         }
     }
 }
