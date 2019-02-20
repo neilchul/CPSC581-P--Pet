dfedaea2dd0ed686342d3f04f6196d1a98d83961
Neil Chulpongsatorn <neil_ninja@hotmail.com>
2019-02-20 14:50:24 -0700

    added head turn

--- Project1-Pet/MainPage.xaml.cs
+++ Project1-Pet/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
 
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Project1_Pet
@@ -39,14 +40,19 @@ namespace Project1_Pet
             };
         }
 
+        double toDegrees(double radians)
+        {
+            return (180/Math.PI) * radians;
+        }
+
         void OnPointerPressed(object sender, PointerRoutedEventArgs e)
         {
-
+           
+            //switch face
             this.IdleShiba.Visibility =  Windows.UI.Xaml.Visibility.Collapsed;
             this.HappyShiba.Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-          
-
+            //red dot
             (this.touchPoint.RenderTransform as CompositeTransform).TranslateX = e.GetCurrentPoint(this.Container).Position.X - this.touchPoint.Width / 2;
             (this.touchPoint.RenderTransform as CompositeTransform).TranslateY = e.GetCurrentPoint(this.Container).Position.Y - this.touchPoint.Height / 2;
 
@@ -56,11 +62,13 @@ namespace Project1_Pet
             {
                 this.Container.Children.Add(this.touchPoint);
             }
+
         }
 
         void OnPointerReleased(object sender, PointerRoutedEventArgs e)
         {
 
+            //switch face back
             this.IdleShiba.Visibility = Windows.UI.Xaml.Visibility.Visible;
             this.HappyShiba.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
 
@@ -69,9 +77,25 @@ namespace Project1_Pet
 
         void OnPointerMoved(object sender, PointerRoutedEventArgs e)
         {
-            (this.touchPoint.RenderTransform as CompositeTransform).TranslateX = e.GetCurrentPoint(this.Container).Position.X - this.touchPoint.Width / 2;
-            (this.touchPoint.RenderTransform as CompositeTransform).TranslateY = e.GetCurrentPoint(this.Container).Position.Y - this.touchPoint.Height / 2;
+            var pointX = e.GetCurrentPoint(this.Container).Position.X;
+            var pointY = e.GetCurrentPoint(this.Container).Position.Y;
 
+            (this.touchPoint.RenderTransform as CompositeTransform).TranslateX = pointX - this.touchPoint.Width / 2;
+            (this.touchPoint.RenderTransform as CompositeTransform).TranslateY = pointY - this.touchPoint.Height / 2;
+
+            //rotating pupper face
+            var newX = (pointX - (66 + (this.HappyShiba.Width / 2)));
+            var newY = (pointY - (384 + (this.HappyShiba.Height / 2)));
+            var powX = Math.Pow(newX, 2);
+            var powY = Math.Pow(newY, 2);
+            var distance = Math.Sqrt(powX + powY);
+            var result = newX / distance;
+
+            var angle = toDegrees(Math.Asin(result));
+
+            RotateTransform rotateTransform = new RotateTransform();
+            rotateTransform.Angle = angle;
+            this.HappyShiba.RenderTransform = rotateTransform;
         }
     }
 }
